ff0589e905ef1a4ef317c32de07147674f2a2d8b
hipeoplea <hipeoplea@yandex.ru>
2026-04-15 17:43:44 +0300

    Revert "mini fix"
    This reverts commit 8a019f09601f1d9b7f9f95e8be33547efa261cff.

--- src/main/kotlin/ru/chousik/kt_blps/controller/YooKassaWebhookController.kt
+++ src/main/kotlin/ru/chousik/kt_blps/controller/YooKassaWebhookController.kt
@@ -1,23 +1,25 @@
 package ru.chousik.kt_blps.controller
 
-import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import ru.chousik.kt_blps.service.PaymentWebhookService
 
-
 @RestController
-@RequestMapping("/api/payments")
+@RequestMapping("/payments/yookassa")
 class YooKassaWebhookController(
     private val paymentWebhookService: PaymentWebhookService
 ) {
-
     @PostMapping("/webhook")
-    @ResponseStatus(HttpStatus.OK)
-    fun handleWebhook(@RequestBody payload: String) {
-        paymentWebhookService.handleWebhook(payload)
+    fun handleWebhook(
+        @RequestBody payload: String,
+        @RequestHeader(value = "X-Forwarded-For", required = false) forwardedFor: String?,
+        @RequestHeader(value = "X-Real-IP", required = false) realIp: String?
+    ): ResponseEntity<Unit> {
+        paymentWebhookService.handleWebhook(payload, forwardedFor, realIp)
+        return ResponseEntity.ok().build()
     }
 }
